8d6fedfb1b1596f9214061c9aa55de4e4ebcf74f
Pratik Rajesh Sampat <psampat2@illinois.edu>
2024-02-19 17:06:04 -0600

    get k8s pod information
    Signed-off-by: Pratik Rajesh Sampat <psampat2@illinois.edu>

--- worker.py
+++ worker.py
@@ -1,7 +1,19 @@
 ## Worker python program that extracts K8s CPU related metrics
 
+import subprocess
+
+def get_pod_information():
+    p = subprocess.run(['kubectl', 'get', 'pods', f'-n=default',
+                        r'-o=jsonpath={range .items[*]}{.metadata.uid} {.metadata.name}{"\n"}{end}'],
+                        stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True, check=True)
+    for i in p.stdout.splitlines():
+        uid, name = i.split()
+        name = name.rsplit('-', 2)[0]
+        print(uid, name)
+
 def main():
     print("Hello world")
+    get_pod_information()
 
 
 if __name__ == "__main__":
