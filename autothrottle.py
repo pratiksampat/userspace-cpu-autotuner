082c15700f15882b21dd1d618a1f3d277ad2d14c
Pratik Rajesh Sampat <psampat2@illinois.edu>
2024-04-08 00:19:53 -0500

    Tower implementation

--- autothrottle.py
+++ autothrottle.py
@@ -18,6 +18,9 @@ import os
 import threading
 import time
 import random
+import collections
+import vowpalwabbit
+import numpy
 
 ## Possible throttle targets
 # 0.00, 0.02, 0.04, 0.06, 0.10, 0.15, 0.20, 0.25, and 0.30
@@ -70,6 +73,138 @@ class AutoThrottle:
         self.action = None
         self.filename = "/home/docker/wrk2/record.log"
 
+        self.last_rps = None
+        self.last_action = None
+        self.last_action_p = None
+
+        # Variables needed for tower
+        self.aggregate_samples = 20;
+        self.learning_rate = 0.5
+
+    def tower(self, containers):
+
+        # Update the samples with something new
+        if (self.last_action is not None):
+            #Extract last RPS and Latency
+            line = subprocess.check_output(['tail', '-1', self.filename]).strip()
+            latency, rps = line.decode().split(';')
+
+            #Extract allocation
+            utils.get_util_info(containers)
+            for cont in containers:
+                period = float(containers[cont]['period'])
+                if (containers[cont]['quota'] == 'max'):
+                    p = subprocess.run(f"nproc", shell=True, capture_output=True, text=True)
+                    quota = (int(p.stdout) * period) / 1000000
+                else:
+                    quota = float(containers[cont]['quota']) / 1000000
+
+            self.samples.append((
+                self.last_rps,
+                self.last_action,
+                self.last_action_p,
+                float(latency),
+                quota/period,
+            ))
+
+        train_samples = list(self.samples)
+        try:
+            min_allocation = min(i[4] for i in train_samples if i[3] <= self.slo)
+            max_allocation = max(i[4] for i in train_samples if i[3] <= self.slo)
+        except ValueError:
+            min_allocation = None
+            max_allocation = None
+        try:
+            min_latency = min(i[3] for i in train_samples if i[3] > self.slo)
+            max_latency = max(i[3] for i in train_samples if i[3] > self.slo)
+        except ValueError:
+            min_latency = None
+            max_latency = None
+        # Compute Cost
+        for i, (rps, action, action_p, latency, allocation) in enumerate(train_samples):
+            if latency <= self.slo:
+                try:
+                    cost = (allocation - min_allocation) / (max_allocation - min_allocation)
+                except ZeroDivisionError:
+                    cost = 0.5
+            else:
+                try:
+                    cost = (latency - min_latency) / (max_latency - min_latency) + 2
+                except ZeroDivisionError:
+                    cost = 2.5
+            train_samples[i] = (rps, action, action_p, cost)
+        print("Train samples ", train_samples)
+
+        def median(l):
+            l = sorted(l)
+            if not l:
+                return None
+            if len(l) % 2:
+                return l[len(l) // 2]
+            else:
+                return (l[len(l) // 2 - 1] + l[len(l) // 2]) / 2
+
+        sample_categories = collections.defaultdict(lambda: collections.defaultdict(list))
+        for i in train_samples:
+            action = i[1]
+            rps = round(float(i[0]) / self.aggregate_samples) * self.aggregate_samples
+            sample_categories[action][rps].append(i)
+
+        # Note this is different from self.aggregate samples
+        aggregated_samples = []
+        for action in sample_categories:
+            for rps in sample_categories[action]:
+                aggregated_samples.append((rps, action, 1 / len(self.targets) ** 2, median(i[3] for i in sample_categories[action][rps])))
+
+        train_samples = []
+        if aggregated_samples:
+            for i in range(1000):
+                train_samples.append(random.choice(aggregated_samples))
+
+        # RL - multi-armed bandits
+        # Features are action, cost and action_p -> Against rps
+        vw = vowpalwabbit.Workspace(f'--cb_explore {len(self.targets) ** 2} --epsilon 0 -l {self.learning_rate} --nn 3 --quiet')
+        for rps, action, action_p, cost in train_samples:
+            vw.learn(f'{action+1}:{cost}:{action_p} | rps:{rps}')
+
+        # Predict for the current rps that it sees
+        line = subprocess.check_output(['tail', '-1', self.filename]).strip()
+        latency, rps = line.decode().split(';')
+        distribution = vw.predict(f'| rps:{rps}')
+
+        action = numpy.random.choice(len(distribution), p=numpy.array(distribution) / sum(distribution))
+        action_p = distribution[action]
+
+        vw.finish()
+
+        if action_p == 1:
+            distribution = [0] * len(self.targets) ** 2
+            distribution[action] += 1 - self.explore
+            explore_actions = []
+            x = action // len(self.targets)
+            y = action % len(self.targets)
+
+            if x - 1 >= 0:
+                explore_actions.append(action - len(self.targets))
+            if x + 1 < len(self.targets):
+                explore_actions.append(action + len(self.targets))
+            if y - 1 >= 0:
+                explore_actions.append(action - 1)
+            if y + 1 < len(self.targets):
+                explore_actions.append(action + 1)
+            for i in explore_actions:
+                distribution[i] += self.explore / len(explore_actions)
+            action = numpy.random.choice(len(distribution), p=numpy.array(distribution) / sum(distribution))
+            action_p = distribution[action]
+
+        self.last_rps = rps
+        self.last_action = action
+        self.last_action_p = action_p
+
+        self.target = self.targets[action // len(self.targets)]
+        print("=====Tower predicted target: =====", self.target)
+
+
     def warmup_cont(self, containers):
         # Don't do anything for warmup amount of time
         if (self.stage < 0):
@@ -102,7 +237,7 @@ class AutoThrottle:
 
             #Extract last RPS and Latency
             line = subprocess.check_output(['tail', '-1', self.filename]).strip()
-            latency, rps = line.decodes.split(';')
+            latency, rps = line.decode().split(';')
 
             # Extract CPU limits for that container
             utils.get_util_info(containers)
@@ -112,90 +247,45 @@ class AutoThrottle:
                     p = subprocess.run(f"nproc", shell=True, capture_output=True, text=True)
                     quota = (int(p.stdout) * period) / 1000000
                 else:
-                    quota = float(containers[cont]['quota']) / 1000000
+                    quota = float(containers[cont]['quota'])
 
-            self.samples.append((
-                float(rps),
-                self.action,
-                action_p,
-                float(latency),
-                quota/period,
-            ))
+
+            if (self.last_action is not None):
+                self.samples.append((
+                    self.last_rps,
+                    self.last_action,
+                    self.last_action_p,
+                    float(latency),
+                    quota/period,
+                ))
+
+            self.last_rps = float(rps)
+            self.last_action = self.action
+            self.last_action_p = action_p
 
             print(self.samples)
 
 
 
-    # def captain_thread(self, containers):
-    #     while True:
-    #         self.captain(containers)
-    #         self.past_alloc.append(self.limit)
-    #         time.sleep(5)
+    def captain_thread(self, containers):
+        while True:
+            self.captain(containers)
+            # self.past_alloc.append(self.limit)
+            time.sleep(5)
 
     def autotune(self, containers):
-        pass
-        # Executes every N=10 periods. TODO: Run this in background
-        # while True:
-        #     self.captain(containers)
-        #     # os.sleep(1) # 10 periods of 100 ms each
 
-    #     # Runs every second - docker stats takes approx 1 second to finish
-    #     ct = threading.Thread(target=self.captain_thread, args=(containers,), daemon = True)
-    #     ct.start()
+        # # Runs every 5 seconds
+        ct = threading.Thread(target=self.captain_thread, args=(containers,), daemon = True)
+        ct.start()
 
-    #     # Main thread logic
-    #     while True:
-    #         # Execute your main thread logic here
-    #         self.tower(containers)
-    #         time.sleep(60)
+        # Main thread logic
+        while True:
+            # Execute your main thread logic here
+            self.tower(containers)
+            time.sleep(60)
 
 
-    # def get_rps_stats(self, file_path, file_position):
-    #     rps_list = []
-
-    #     try:
-    #         with open(file_path, 'r') as file:
-    #             file.seek(file_position)
-    #             # Read remaining lines and get second column information
-    #             for line in file:
-    #                 columns = line.strip().split(';')
-    #                 if len(columns) >= 2:
-    #                     rps_list.append(float(columns[0]))
-
-    #             # Save the current file position
-    #             file_position = file.tell()
-    #     except:
-    #         print("Nothing more to read")
-
-    #     return rps_list, file_position
-
-    # def greater_than_slo(self, x):
-    #     return x > self.slo
-
-
-    def tower(self, containers):
-        pass
-        # train_samples = []
-        # latency_list, self.file_location = self.get_rps_stats("record.log", self.file_location)
-        # print(latency_list)
-        # print(self.file_location)
-
-        # latency_mean = statistics.mean(latency_list)
-        # latency_99 = statistics.quantiles(latency_list, n=100)[-1]
-        # print(latency_mean, latency_99)
-
-        # # Get SLO violation list
-        # latency_violation = list(filter(self.greater_than_slo, latency_list))
-        # latency_min = min(latency_violation)
-        # latency_max = max(latency_violation)
-        # alloc_min = min(self.past_alloc)
-        # alloc_max = max(self.past_alloc)
-
-        # for lat in latency_list:
-            
-        # # Get Cost
-
-        # train_samples.append({rps, action, action_p, cost})
 
     def captain(self, containers):
         print("Captain learning")
